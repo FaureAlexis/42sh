5a45f30408a9467aed8651cdf5df051f14f6e0c0
Alexis Faure <alexis.faure@epitech.eu>
2022-05-09 13:11:40 +0200

    [ADD] :rocket: --> Add shell loop function

---
+++ src/shell.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** 42sh
+** File description:
+** shell loop
+*/
+
+#include "42sh.h"
+
+int shell(char **env)
+{
+    if (!env)
+        return FAILURE;
+    return SUCCESS;
+}
