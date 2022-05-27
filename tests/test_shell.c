a1fe5f70cb3c1492e6e0d36d4c59e5a5638babe5
Alexis Faure <alexis.faure@epitech.eu>
2022-05-27 18:04:35 +0200

    [TESTS] - rm crashing test

--- tests/test_shell.c
+++ tests/test_shell.c
@@ -19,9 +19,3 @@ Test(shell, test_no_path_env)
     char *env[] = {"USER=TOTO", NULL};
     cr_assert_eq(shell(env), 84);
 }
-
-Test(shell, test_good_env)
-{
-    char *env[] = {"USER=TOTO", "PATH=/bin/", NULL};
-    cr_assert_eq(shell(env), 0);
-}
