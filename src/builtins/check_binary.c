2c6ac1f11ea1dba1bc140c8f9202e7d9fad362af
Alexis Faure <alexis.faure@epitech.eu>
2022-05-27 15:24:54 +0200

    [FIX] - Fix function ptr array

--- src/builtins/check_binary.c
+++ src/builtins/check_binary.c
@@ -26,8 +26,8 @@ int call_binary(char *binary, char **args, shell_t *shell)
         return FAILURE;
     int (*func_list[7])(char **args, shell_t *shell) = {&my_cd, &my_env,
         &my_setenv, &my_unsetenv, &my_theme, &my_alias, &my_history};
-    char *list[] = {"exit", "cd", "env", "setenv", "unsetenv", "theme",
-        "alias", "history", NULL};
+    char *list[] = {
+        "cd", "env", "setenv", "unsetenv", "theme", "alias", "history", NULL};
     for (int idx = 0; list[idx]; idx += 1) {
         if (strcmp(binary, list[idx]) == 0)
             return func_list[idx](args, shell);
